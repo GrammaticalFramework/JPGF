dced91c0be3f775ab896961a1ec50161bc71c9e7
Grégoire Détrez <gdetrez@crans.org>
2010-04-21 14:56:27 +0200

    The old category classes are not needed anymore
    Ignore-this: 69ab5bcba8ef48ae0173017a96e34e38
    
    darcs-hash:20100421125627-e7b0d-b0cb1cb6b4d3167cc5344caaa2f3d2abda4ec37b.gz

--- src/main/java/pgf/parse/pmcfg/Category.java
+++
@@ -1,9 +0,0 @@
-package pgf.parse.pmcfg;
-
-/**
- * The abstract class Category subsume the classes EthernalCategory, a category
- * that exists in the grammar, and FreshCategory, a category created at parse-time.
- * @author gdetrez
- *
- */
-abstract public class Category {}
