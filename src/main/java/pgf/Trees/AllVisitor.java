d7bcde58b6df04aae47e7bb527b20e9eae159f63
Grégoire Détrez <gdetrez@crans.org>
2010-05-24 16:47:19 +0200

    Removing old files
    Ignore-this: 732d18842ff566268ee44c43d89336f1
    
    darcs-hash:20100524144719-e7b0d-2ef0d5fb3a12fc9f754f05e238af75e64b7819d9.gz

--- src/main/java/pgf/Trees/AllVisitor.java
+++
@@ -1,9 +0,0 @@
-package pgf.Trees;
-
-import pgf.Trees.Absyn.*;
-
-/** BNFC-Generated All Visitor */
-public interface AllVisitor<R,A> extends
-  pgf.Trees.Absyn.Tree.Visitor<R,A>,
-  pgf.Trees.Absyn.Lit.Visitor<R,A>
-{}
