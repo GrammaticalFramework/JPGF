5c94cf65d011a59723701bb21b5f5d5a7e2df6d2
Grégoire Détrez <gdetrez@crans.org>
2010-05-25 15:05:37 +0200

    Setting up the app ui
    Ignore-this: a1e93cd8bed57710f3bed0c9f53f175e
    
    darcs-hash:20100525130537-e7b0d-2402fe2791ea0efba9f5e7b1a1fe55c72f335183.gz

--- examples/phrasedroid/src/main/scala/Activity.scala
+++ examples/phrasedroid/src/main/scala/Activity.scala
@@ -3,12 +3,28 @@ package se.chalmers
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
 import _root_.android.widget.TextView
+import _root_.android.widget._
+import _root_.android.view.View
+//import android.view.Menu._
+
 
 class MainActivity extends Activity {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
-    setContentView(new TextView(this) {
-      setText("hello, world")
+    setContentView(R.layout.main)
+
+    // Get pointers to the ui elements
+    val phraseField = findViewById(R.id.phrase).asInstanceOf[EditText]
+    val translateButton =
+      findViewById(R.id.translate_button).asInstanceOf[Button]
+    val resultView = findViewById(R.id.result_view).asInstanceOf[TextView]
+
+    // translate action
+    translateButton.setOnClickListener( new View.OnClickListener() {
+      def onClick(v:View) = {
+        val phrase = phraseField.getText.toString
+        resultView.setText("Your phrase was : " + phrase)
+      }
     })
   }
 }
