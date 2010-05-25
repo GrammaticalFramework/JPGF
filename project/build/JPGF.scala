972759b13bbb8815db8f3be63d3cbb44f38eedff
Grégoire Détrez <gdetrez@crans.org>
2010-05-25 15:58:28 +0200

    Removing dead code
    Ignore-this: 2d9360ebfc5485716fe3ce8e420e2bbb
    
    darcs-hash:20100525135828-e7b0d-6fbc51cb58d08f8dacbb829c9f29e2e56c4c0704.gz

--- project/build/JPGF.scala
+++ project/build/JPGF.scala
@@ -18,30 +18,6 @@ class JPGFProject(info: ProjectInfo) extends ParentProject(info)
             "FoodsDroid",                  // Project name
             new AndroidApp(_),             // Project class
             library)                       // Dependencies
-
-  // lazy val foodDroid =
-  //   project("examples" / "fooddroid",
-  //           "FoodDroid",
-  //           new FoodApp(_),
-  //           library)
-
-  // lazy val foodDroid =
-  //   project("examples" / "fooddroid", "FoodDroid", new App(_), library)
-
   class AndroidApp(info: ProjectInfo) extends AndroidProject(info)
   with AndroidDefaults
-
-
-
-
-//  lazy val
-//   lazy val ui = project("ui", "Simulator User Interface", // core)
-
-   // lazy val examples = project("examples", "Simulator Examples", new Examples(_))
-
-   // class Examples(info: ProjectInfo) extends ParentProject(info)
-   // {
-   //    lazy val embedded = project("embedded", "Embedded Simulator Example", core)
-   //    lazy val standalone = project("standalone", "Standalone Simulator Example", ui)
-   // }
 }
