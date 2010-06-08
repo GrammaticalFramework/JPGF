dee93886e1c952a420142c11a05b7ef52993e2b8
gdetrez <gdetrez@crans.org>
2010-06-08 10:37:44 +0200

    [Style] Layout of ProductionSet
    Ignore-this: 53929d8dd6c23d27f37891ad542874c2
    
    darcs-hash:20100608083744-dfc49-3baf3dd22f2a543cf8994e2f159873a2d38c2062.gz

--- src/main/java/org/grammaticalframework/reader/ProductionSet.java
+++ src/main/java/org/grammaticalframework/reader/ProductionSet.java
@@ -1,13 +1,14 @@
 package org.grammaticalframework.reader;
 import java.util.HashSet;
-public class ProductionSet {
-private int id;
-private Production[] prods;
 
-public ProductionSet(int _id, Production[] _prods)
-{id = _id;
-prods = _prods;
-}
+public class ProductionSet {
+    private int id;
+    private Production[] prods;
+
+    public ProductionSet(int _id, Production[] _prods) {
+        id = _id;
+        prods = _prods;
+    }
 
     public int length() {
         return this.prods.length;
@@ -17,21 +18,21 @@ prods = _prods;
         return this.prods;
     }
 
-public String toString()
-{String ss = "Id : "+id+" , Productions : [";
-for(int i=0; i<prods.length; i++)
-ss+=(" "+prods[i].toString());
-ss+="]";
-return ss;
-}
+    public String toString() {
+        String ss = "Id : "+id+" , Productions : [";
+        for(int i=0; i<prods.length; i++)
+            ss+=(" "+prods[i].toString());
+        ss+="]";
+        return ss;
+    }
 
-public int getId() {return id; }
-public Production[] getProductions() {return prods;}
+    public int getId() {return id; }
+    public Production[] getProductions() {return prods;}
 
-public HashSet<Production> getSetOfProductions() 
-{HashSet<Production> hs = new HashSet<Production>();
-for(int i=0; i<prods.length; i++)
-	hs.add(prods[i]);
-return hs;
-}
+    public HashSet<Production> getSetOfProductions() {
+        HashSet<Production> hs = new HashSet<Production>();
+        for(int i=0; i<prods.length; i++)
+            hs.add(prods[i]);
+        return hs;
+    }
 }
