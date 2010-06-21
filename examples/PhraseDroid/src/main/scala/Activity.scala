740974ab9451dd84d299d4f48a27d0720a96eff1
Grégoire Détrez <gdetrez@crans.org>
2010-06-21 16:48:21 +0200

    [FIX] removing useless imports in the Activity.scala file
    Ignore-this: d3a54629e8f47152f3c97d317b664e96
    
    darcs-hash:20100621144821-e7b0d-fcceb37386f81a275a5878ea26655c748cece09b.gz

--- examples/PhraseDroid/src/main/scala/Activity.scala
+++ examples/PhraseDroid/src/main/scala/Activity.scala
@@ -1,17 +1,5 @@
 package org.grammaticalframework.examples.PhraseDroid
 
-import _root_.android.app.Activity
-import _root_.android.os.Bundle
-import _root_.android.widget.TextView
-import _root_.android.widget._
-import _root_.android.view._
-import _root_.android.content.res.Resources
-import _root_.android.app.ProgressDialog
 // Debuging
-
-import _root_.org.grammaticalframework.reader.{NewReader, PGF, Concrete}
-import _root_.org.grammaticalframework.parser.{Parser}
-import _root_.org.grammaticalframework.linearizer.Linearizer
-import _root_.org.grammaticalframework.Trees.PrettyPrinter
 
 class MainActivity extends PhrasedroidActivity
