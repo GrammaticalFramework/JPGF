fece4768d2be0fbd332e6e0c0a03f2b904a7a7f8
Grégoire Détrez <gdetrez@crans.org>
2010-06-28 16:12:11 +0200

    Removing unecessary stuff in the monolingual phrasedroid app
    Ignore-this: 3a9092871d638d2d2c784a5b90fc6dfd
    
    darcs-hash:20100628141211-e7b0d-4535be0070263ada66573df882914532d75ea7a7.gz

--- examples/PhraseDroid-eng2fre/src/main/java/org/grammaticalframework/examples/phrasedroid/R.java
+++ examples/PhraseDroid-eng2fre/src/main/java/org/grammaticalframework/examples/phrasedroid/R.java
@@ -26,16 +26,6 @@ public final class R {
     }
     public static final class raw {
         public static final int phrasebook=0x7f040000;
-        public static final int phrasebook_de_en=0x7f040001;
-        public static final int phrasebook_de_es=0x7f040002;
-        public static final int phrasebook_de_fr=0x7f040003;
-        public static final int phrasebook_de_it=0x7f040004;
-        public static final int phrasebook_en_es=0x7f040005;
-        public static final int phrasebook_en_fr=0x7f040006;
-        public static final int phrasebook_en_it=0x7f040007;
-        public static final int phrasebook_es_fr=0x7f040008;
-        public static final int phrasebook_es_it=0x7f040009;
-        public static final int phrasebook_fr_it=0x7f04000a;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
