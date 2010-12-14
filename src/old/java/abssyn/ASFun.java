d19e6b0fd8c909a6668f01fffe2c0927ed72a5a7
Grégoire Détrez <gdetrez@crans.org>
2010-12-14 20:26:47 +0100

    Removing old source files

--- src/old/java/abssyn/ASFun.java
+++
@@ -1,9 +0,0 @@
-package se.chalmers.cs.pgf.abssyn;
-
-public class ASFun extends ASTree {
-
-	private String name;
-	private ASTree[] children;
-	
-	
-}
--- src/old/java/org/gf/Pair.java
+++
@@ -1,7 +0,0 @@
-package org.gf;
-
-class Pair<A,B> {
-	public A fst;
-	public B snd;
-
-}
